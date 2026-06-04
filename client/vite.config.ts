5357e38c917a91eae06bcfa8a5e04fe88a9e18c9
Francois Goessens <goessens.francois97@gmail.com>
2026-06-04 16:04:29 +0200

    FGS - 04/06/26 - Deploying Github Actions

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
+  base: '/pokeidle/',
   plugins: [vue(), vuetify({ autoImport: true })],
 })
